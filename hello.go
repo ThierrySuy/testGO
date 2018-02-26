74b2b4c9d7d8eb1d38789ff199d726d1b57ff4f2
ThierrySuy <tsuy@accengage.com>
2018-02-26 17:37:39 +0100

    version 2

---
+++ hello.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("go" + "lang \n")
+	fmt.Println("land" + "go")
+	//Integers and floats.
+	
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	//Booleans, with boolean operators as you’d expect.
+	
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+	//var age int = 25
+	//var name string = "Thierry"
+
+	fmt.Println("name" + "a" + "age")
+
+}
